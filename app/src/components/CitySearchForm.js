e7d7c2394ea2f5178cca726779ae78826dcc9110
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-04-06 13:11:39 +0100

    draft city search form function added

---
+++ app/src/components/CitySearchForm.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function CitySearchForm(props) {
+    return (
+      <form>
+        <div className="form-group">
+          <label htmlFor="search">Search:</label>
+          <input
+            value={props.search}
+            name="search"
+            type="text"
+            className="form-control"
+            placeholder="Enter City Here..."
+            id="search"
+          />
+          <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
+            Search
+          </button>
+        </div>
+      </form>
+    );
+  }
